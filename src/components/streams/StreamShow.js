f7d6a24fb6445e70a338e6f9f595a73ab20d3005
tanld7 <tanld.dev@gmail.com>
2022-01-24 06:51:34 -0800

    Implement StreamShow

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,6 +1,7 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useRef} from 'react'
 import { connect, useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import flv from 'flv.js';
 
 import { fetchStream } from "../../actions";
 
@@ -10,12 +11,37 @@ const StreamShow = () => {
     const dispatch = useDispatch();
     const { id } = useParams();
     const stream = useSelector((state) => state.streams[id])
+    const videoRef = useRef();
+
+    // Initialize player object with the id
+    const player = flv.createPlayer({
+        type: 'flv',
+        url: `http://localhost:8000/live/${id}.flv`
+    })
 
     useEffect(() => {
+        console.log(videoRef)
+        console.log(player)
+        console.log(id)
+
         if (!stream) {
             dispatch(fetchStream(id));
         }
-    }, [])
+
+        // Attempt to build player
+        buildPlayer()
+    }, [player])
+
+    const buildPlayer = () => {
+        // If we already have the player (built before) or the stream object is not ready,
+        // we're not gonna attach the video to component.
+        if (player || !stream) {
+            return;
+        }
+        player.attachMediaElement(videoRef.current);
+        player.load();
+        player.play();
+    }
 
     if(!stream) {
         return <div>Loading ...</div>
@@ -23,6 +49,7 @@ const StreamShow = () => {
 
     return (
         <div>
+            <video ref={videoRef} style={{width: '100%'}} controls={true} />
             <h1>{stream.title}</h1>
             <h5>{stream.description}</h5>
         </div>
